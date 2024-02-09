a0e3f82fbebd0fdb2ebc82af84923a5fe1ddf43d
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2024-02-09 18:28:36 +0000

    Updating to latest FlutterFlow output.

--- lib/custom_code/actions/g_subscribe.dart
+++ lib/custom_code/actions/g_subscribe.dart
@@ -18,7 +18,7 @@ Future<EegStruct> gSubscribe(String cortexToken, String headsetId) async {
   final wsUrl = Uri.parse('wss://localhost:6868');
   final channel = WebSocketChannel.connect(wsUrl);
 
-  final completer = Completer<String>();
+  final completer = Completer<EegStruct>();
   final List<String> responses = [];
   String sessionId = '';
   bool reachedZero = false;
@@ -38,9 +38,8 @@ Future<EegStruct> gSubscribe(String cortexToken, String headsetId) async {
   eeg.f8 = [];
   eeg.af4 = [];
 
-  try {
-    // CREATE SESSION
-    channel.sink.add('''
+  // CREATE SESSION
+  channel.sink.add('''
       {
         "id": 9,
         "jsonrpc": "2.0",
@@ -53,15 +52,15 @@ Future<EegStruct> gSubscribe(String cortexToken, String headsetId) async {
       }
     ''');
 
-    // SUBSCRIBE
-    print('gSubscribe: Subscribe start');
+  // SUBSCRIBE
+  print('gSubscribe: Subscribe start');
 
-    // Forward messages from subscribeChannel to the new stream
-    channel.stream.listen((message) {
-      if (sessionId.isEmpty) {
-        sessionId = getSessionId(message);
-        print(message);
-        channel.sink.add('''
+  // Forward messages from subscribeChannel to the new stream
+  channel.stream.listen((message) {
+    if (sessionId.isEmpty) {
+      sessionId = getSessionId(message);
+      print(message);
+      channel.sink.add('''
           {
             "id": 10,
             "jsonrpc": "2.0",
@@ -73,46 +72,41 @@ Future<EegStruct> gSubscribe(String cortexToken, String headsetId) async {
             }
           }
         ''');
-      } else {
-        //"cols":["COUNTER","INTERPOLATED","AF3","F7","F3","FC5","T7","P7","O1","O2","P8","T8","FC6","F4","F8","AF4","RAW_CQ","MARKER_HARDWARE","MARKERS"]
-        print('gSubscribe: $reachedZero Received message: $message');
-        final Map<String, dynamic> json = jsonDecode(message);
-        if (json.containsKey("eeg")) {
-          print('json[eeg][0]: ${json["eeg"][0]}');
-          if (!reachedZero && json["eeg"][0] == 0) reachedZero = true;
-          if (reachedZero) {
-            responses.add(message);
-            eeg.af3.add(json["eeg"][2]);
-            eeg.f7.add(json["eeg"][3]);
-            eeg.f3.add(json["eeg"][4]);
-            eeg.fc5.add(json["eeg"][5]);
-            eeg.t7.add(json["eeg"][6]);
-            eeg.p7.add(json["eeg"][7]);
-            eeg.o1.add(json["eeg"][8]);
-            eeg.o2.add(json["eeg"][9]);
-            eeg.p8.add(json["eeg"][10]);
-            eeg.t8.add(json["eeg"][11]);
-            eeg.fc6.add(json["eeg"][12]);
-            eeg.f4.add(json["eeg"][13]);
-            eeg.f8.add(json["eeg"][14]);
-            eeg.af4.add(json["eeg"][15]);
-          }
-          print("responses.length: ${responses.length}");
-          if (responses.length >= 128) {
-            print("eeg.af3.length: ${eeg.af3.length}");
-            print("eeg.af3.first: ${eeg.af3.first}");
-            print(
-                'gSubscribe: Reached 128 records. Closing WebSocket channel.');
-            channel.sink.close(status.goingAway);
-          }
+    } else {
+      //"cols":["COUNTER","INTERPOLATED","AF3","F7","F3","FC5","T7","P7","O1","O2","P8","T8","FC6","F4","F8","AF4","RAW_CQ","MARKER_HARDWARE","MARKERS"]
+      print('gSubscribe: $reachedZero Received message: $message');
+      final Map<String, dynamic> json = jsonDecode(message);
+      if (json.containsKey("eeg")) {
+        print('json[eeg][0]: ${json["eeg"][0]}');
+        if (!reachedZero && json["eeg"][0] == 0) reachedZero = true;
+        if (reachedZero) {
+          responses.add(message);
+          eeg.af3.add(json["eeg"][2]);
+          eeg.f7.add(json["eeg"][3]);
+          eeg.f3.add(json["eeg"][4]);
+          eeg.fc5.add(json["eeg"][5]);
+          eeg.t7.add(json["eeg"][6]);
+          eeg.p7.add(json["eeg"][7]);
+          eeg.o1.add(json["eeg"][8]);
+          eeg.o2.add(json["eeg"][9]);
+          eeg.p8.add(json["eeg"][10]);
+          eeg.t8.add(json["eeg"][11]);
+          eeg.fc6.add(json["eeg"][12]);
+          eeg.f4.add(json["eeg"][13]);
+          eeg.f8.add(json["eeg"][14]);
+          eeg.af4.add(json["eeg"][15]);
+        }
+        print("responses.length: ${responses.length}");
+        if (responses.length >= 2) {
+          print("eeg.af3.length: ${eeg.af3.length}");
+          print("eeg.af3.first: ${eeg.af3.first}");
+          completer.complete(eeg);
+
+          print('gSubscribe: Reached 128 records. Closing WebSocket channel.');
+          channel.sink.close(status.goingAway);
         }
       }
-    });
-    completer.complete(responses.join('\n'));
-  } catch (error) {
-    print('gSubscribe: Error occurred: $error');
-    completer.completeError(error);
-  }
-
-  return eeg;
+    }
+  });
+  return await completer.future;
 }
